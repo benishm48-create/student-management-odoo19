32b13ef592bc594096df3a68fc53e07734f90cbe
Benish M <benishm48@gmail.com>
2026-09-06 21:47:08 +0530

    Fix Railway Odoo port

--- render-start.sh
+++ render-start.sh
@@ -2,8 +2,9 @@
 
 odoo \
   --http-interface=0.0.0.0 \
-  --http-port=${PORT:-10000} \
+  --http-port=8069 \
   --db_host=${DB_HOST} \
   --db_port=${DB_PORT:-5432} \
   --db_user=${DB_USER} \
-  --db_password=${DB_PASSWORD}
+  --db_password=${DB_PASSWORD} \
+  --database=${DB_NAME}
